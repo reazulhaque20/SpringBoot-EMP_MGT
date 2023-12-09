6dab3eef1578fe4c61d1ad53d2002bedcd52680e
Md. Reazul Haque <37473674+reazulhaque20@users.noreply.github.com>
2023-12-10 00:54:15 +0600

    Project Basic files

---
+++ src/main/java/com/rhaque/EmpMgtApplication.java
@@ -0,0 +1,13 @@
+package com.rhaque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpMgtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpMgtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rhaque/EmpMgtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rhaque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpMgtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
